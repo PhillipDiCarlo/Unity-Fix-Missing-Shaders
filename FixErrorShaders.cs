480fa108a698831ff0d890f59a89da1c11bc0110
Phillip DiCarlo <sasha-1221@hotmail.com>
2024-11-02 23:56:23 -0400

    Updated Menu

--- FixErrorShaders.cs
+++ FixErrorShaders.cs
@@ -3,7 +3,7 @@ using UnityEditor; // This namespace is necessary for EditorWindow and MenuItem
 
 public class FixErrorShaders : EditorWindow
 {
-    [MenuItem("Tools/Fix Error Shaders")] // Adds an item to the Unity Editor menu
+    [MenuItem("Tools/Italiandogs/Fix Error Shaders")] // Adds an item to the Unity Editor menu
     public static void ShowWindow()
     {
         GetWindow<FixErrorShaders>("Fix Error Shaders");
